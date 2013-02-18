02e9d3de35d678d60687c90fe132852cb9e34a08
Leon Coto <leoncoto@gmail.com>
2013-02-18 08:33:22 +0000

    First transition nailed correctly

--- www/js/app/second-cube.js
+++ www/js/app/second-cube.js
@@ -5,15 +5,15 @@ define(function(require) {
         faces = require('app/face-stream'),
         tmpl = require('templates')['cube-face'],
         ctx = require('app/rendering-context')(150),
-        $cube = $('#cube')
+        $cubes = $('#cubes')
 
     var init = function () {
       var firstFace = faces(tmpl, ctx)
-      $cube.append(firstFace.render())
-      $cube.append(firstFace.next().render())
-      $cube.append(firstFace.next().next().render())
-      $cube.append(firstFace.next().next().next().render())
-      $cube.append(firstFace.next().next().next().next().render())
+      $cubes.append(firstFace.render())
+      $cubes.append(firstFace.next().render())
+      $cubes.append(firstFace.next().next().render())
+      $cubes.append(firstFace.next().next().next().render())
+      $cubes.append(firstFace.next().next().next().next().render())
     }
 
     $(function () {
